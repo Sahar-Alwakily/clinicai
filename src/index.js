914f37204076728ef02ab03090b8522df2a2d220
Sahar Alwakily <33603273+Sahar-Alwakily@users.noreply.github.com>
2025-12-17 14:59:10 +0200

    بي

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  * @Description:
  */
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import store from "./store/";
@@ -16,11 +16,11 @@ import App from "./App";
 
 import "assets/styles/reset";
 
-ReactDOM.render(
+const root = createRoot(document.querySelector("#root"));
+root.render(
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>,
-  document.querySelector("#root")
+  </Router>
 );
